3978eae4b24cf5b3c9277ff11ec09597d180c3db
zuowutan <yijiutanzuowu1992@163.com>
2018-12-04 15:54:24 +0800

    commit content

--- variableStu.py
+++ variableStu.py
@@ -5,71 +5,108 @@
 #字典的定义
 
 # 使用前就定义字典
-endDict = {'name': 'tom', 'code': "Python", 'profession': 'Coder'}
-# 输出完整的字典  {'code': 'Python', 'profession': 'Coder', 'name': 'tom'}
-print endDict
+# endDict = {'name': 'tom', 'code': "Python", 'profession': 'Coder'}
+# # 输出完整的字典  {'code': 'Python', 'profession': 'Coder', 'name': 'tom'}
+# print endDict
+#
+# # 输出所有键  ['code', 'profession', 'name']
+# print endDict.keys()
+#
+# # 输出code键 找到对应的value  Python
+# print endDict['code']
+#
+# # 输出所有属性值  ['Python', 'Coder', 'tom']
+# print endDict.values()
+#
+#
+#
+#
+# dictData = { }
+# dictData['key1'] = "key1 对应的属性值"
+# dictData[2] = "2对应的属性值"
+# dictData[3] = True
+# dictData['hello'] = "我爱你中国"
+# dictData['word'] = 520.1314
+#
+# print dictData['hello']          # 输出键为'hello' 的值    我爱你中国
+# print dictData['word']           # 输出键为 word 的值   520.1314
+#
+#
+#
+# #字典的定义
+# print ("---------字典-------------")
+#
+#
+#
+#
+# #元组的定义
+# print ("---------元组-------------")
+# tupleData = (True, "False", 520, 13.14)
+# smallTupleData = (456, 'Tom')
+# # 元组不支持设置 属性值 否则会报错  TypeError: 'tuple' object does not support item assignment
+#
+# print tupleData[0]
+#
+# # 输出完整元组  (True, 'False', 520, 13.14)
+# print tupleData
+#
+# # 输出元组的第一个元素  True
+# print tupleData[0]
+#
+# # 输出第二个至第三个的元素  ('False', 520)
+# print tupleData[1:3]
+#
+# # 输出从第三个开始至列表末尾的所有元素  (520, 13.14)
+# print tupleData[2:]
+#
+# # 输出元组两次  (True, 'False', 520, 13.14, True, 'False', 520, 13.14)
+# print tupleData * 2
+#
+# # 打印组合的元组  (True, 'False', 520, 13.14, 456, 'Tom')
+# print tupleData + smallTupleData
+#
+# print ("---------元组-------------")
 
-# 输出所有键  ['code', 'profession', 'name']
-print endDict.keys()
+#定义元组
+data = (2,3)
+newData = (4,5)
 
-# 输出code键 找到对应的value  Python
-print endDict['code']
+# 构建新的元组
+endData = data + newData
 
-# 输出所有属性值  ['Python', 'Coder', 'tom']
-print endDict.values()
+# 最终打印：(2, 3, 4, 5)
+print endData
 
-
-
-
-dictData = { }
-dictData['key1'] = "key1 对应的属性值"
-dictData[2] = "2对应的属性值"
-dictData[3] = True
-dictData['hello'] = "我爱你中国"
-dictData['word'] = 520.1314
-
-print dictData['hello']          # 输出键为'hello' 的值    我爱你中国
-print dictData['word']           # 输出键为 word 的值   520.1314
+# 截取endData元组里面的索引为1极其以后的数据
+subsData = endData[1:]
+# 会打印：(3, 4, 5)
+print subsData
 
 
-
-#字典的定义
-print ("---------字典-------------")
-
-
-
-
-#元组的定义
-print ("---------元组-------------")
-tupleData = (True, "False", 520, 13.14)
-smallTupleData = (456, 'Tom')
-# 元组不支持设置 属性值 否则会报错  TypeError: 'tuple' object does not support item assignment
-
-print tupleData[0]
-
-# 输出完整元组  (True, 'False', 520, 13.14)
-print tupleData
-
-# 输出元组的第一个元素  True
-print tupleData[0]
-
-# 输出第二个至第三个的元素  ('False', 520)
-print tupleData[1:3]
-
-# 输出从第三个开始至列表末尾的所有元素  (520, 13.14)
-print tupleData[2:]
-
-# 输出元组两次  (True, 'False', 520, 13.14, True, 'False', 520, 13.14)
-print tupleData * 2
-
-# 打印组合的元组  (True, 'False', 520, 13.14, 456, 'Tom')
-print tupleData + smallTupleData
-
-print ("---------元组-------------")
-
-#列表的定义
 listData = [True, "False", 233, 12.3]
 smallListData = [123, 'john']
+
+# 删除具体的元素
+del listData[1]
+del listData[2]
+
+# 打印具体的属性值 [True, 233]
+print listData
+
+
+
+
+# 添加单个元素
+listData.append("Python")
+# 输出：   [True, 'False', 233, 12.3, 'Python']
+print listData
+
+
+# 添加子列表
+listData.append(smallListData)
+# 输出：   [True, 'False', 233, 12.3, 'Python', [123, 'john']]
+print listData
+
 
 # 输出完整列表   [True, 'False', 233, 12.3]
 print listData
